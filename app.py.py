3c39608fd6db8631e141f71428b2e9ac5e908e96
Adelaide Odametey <royaladelaideodametey@gmail.com>
2024-11-19 10:25:49 +0000

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -10,7 +10,7 @@ except Exception as e:
     st.stop()
 
 # Display the logo
-#st.image('bml_logo.png', width=200)
+st.image('bml_logo.png', width=200)
 
 # Display the heading
 st.title("BML Group")
